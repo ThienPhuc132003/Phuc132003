a4789b3f6e2d821fc3c50ef341ff6ee5b8bad036
ThienPhuc132003 <127500862+ThienPhuc132003@users.noreply.github.com>
2024-05-07 00:23:02 +0700

    Add files via upload

---
+++ ECOMMERCIAL-WEB/server/utils/MyConstants.js
@@ -0,0 +1,11 @@
+const MyConstants = {
+    DB_SERVER: 'shopdoannhom.ytq4crj.mongodb.net',
+    DB_USER: 'admin',
+    DB_PASS: 'Admin123',
+    DB_DATABASE: 'DoAnNhom',
+    JWT_SECRET: 'doannhom',
+    JWT_EXPIRES: '36000000', // in milliseconds
+    EMAIL_USER: 'doanskt1genshin@gmail.com', // gmail service
+    EMAIL_PASS: 'awct brkr xvxt vjii'
+  };
+  module.exports = MyConstants;
